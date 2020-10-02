70cab578265b80b4dd206a05422c6cbd07ce644c
nafiginm <nafiginm@yandex.ru>
2020-10-02 18:06:13 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,96 @@
+const quizData = [
+  {
+    question: 'What is the most used programming language in 2019?',
+    a: 'Java',
+    b: 'C',
+    c: 'Python',
+    d: 'JavaScript',
+    correct: 'd'
+  },
+  {
+    question: 'Who is the President of US?',
+    a: 'Ivan Ivanov',
+    b: 'Donald Trump',
+    c: 'Ivan Saldano',
+    d: 'Mihai Andrei',
+    correct: 'b'
+  },
+  {
+    question: 'What does HTML stand for?',
+    a: 'Hypertext Markup Language',
+    b: 'Cascading Style Sheet',
+    c: 'Jason Object Notation',
+    d: 'Application Programming Interface',
+    correct: 'a'
+  },
+  {
+    question: 'What year was JavaScript launched?',
+    a: '1996',
+    b: '1995',
+    c: '1994',
+    d: 'none of the above',
+    correct: 'b'
+  }
+]
+const quiz = document.getElementById('quiz')
+const answers = document.querySelectorAll('.answer')
+const questionEl = document.getElementById('question')
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+const submit = document.getElementById('submit')
+
+let currentQuiz = 0
+let score = 0
+
+loadQuiz()
+
+function loadQuiz() {
+  deSelectAnswers()
+
+  const currentQuizData = quizData[currentQuiz]
+
+  questionEl.innerHTML = currentQuizData.question
+  a_text.innerHTML = currentQuizData.a
+  b_text.innerHTML = currentQuizData.b
+  c_text.innerHTML = currentQuizData.c
+  d_text.innerHTML = currentQuizData.d
+}
+
+function getSelected() {
+  let answer = undefined
+
+  answers.forEach((answerEl) => {
+    if (answerEl.checked) {
+      answer = answerEl.id
+    }
+  })
+
+  return answer
+}
+
+function deSelectAnswers() {
+  answers.forEach((answerEl) => {
+    answerEl.checked = false
+  })
+}
+
+submit.addEventListener('click', () => {
+  const answer = getSelected()
+
+  if (answer) {
+    if(answer === quizData[currentQuiz].correct) {
+      score++
+    }
+    currentQuiz++
+    if (currentQuiz < quizData.length) {
+      loadQuiz()
+    } else {
+      quiz.innerHTML = `
+        <h2>You answered correctly at ${score}/${quizData.length} questions</h2>
+        <button onclick="location.reload()">Reload</button>
+      `
+    }
+  }
+})
